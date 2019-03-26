48e157916a5b8a099ae541c2b555f832c47ba8bb
Komputer <Daniel200825@wp.pl>
2019-03-26 04:38:31 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    static double result = 2.032;
     public static void main(String[] args) {
-        System.out.println("kek"+result);
         Window window = new Window();
         //Flow flow = new Flow();
     }
--- src/Window.java
+++ src/Window.java
@@ -3,107 +3,74 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Window implements ActionListener, ChangeListener {
     JFrame window = new JFrame("interface GUI");
     JButton clear = new JButton("clear");
     JButton insert = new JButton("insert");
     JButton save = new JButton("save");
-    JButton min = new JButton("min");
-    JButton max = new JButton("max");
+    JButton min = new JButton("Minimum");
+    JButton max = new JButton("Maximum");
     JButton average = new JButton("average");
-    JSlider sliderHorizontal = new JSlider(JSlider.HORIZONTAL,0,5,0);
-    JSlider sliderVertical = new JSlider(JSlider.VERTICAL,0,5,0);
+    JSlider sliderHorizontal = new JSlider(JSlider.HORIZONTAL, 0, 4, 0);
+    JSlider sliderVertical = new JSlider(JSlider.VERTICAL, 0, 4, 0);
     JSlider source;
     JLabel textFieldLabel = new JLabel("Podaj wartość: ");
     JLabel textAreaLabel = new JLabel("Wynik działania to: ");
-    JTable kek;
+    JTable table;
     JTextField textField = new JTextField();
     JTextArea textArea = new JTextArea();
+    JTextArea result;
+    int ver = 4;
+    int hor = 0;
     GridBagConstraints c = new GridBagConstraints();
-    public Window(){
+
+    public Window() {
         createWindow();
     }
 
-    public void createWindow(){
+    public void createWindow() {
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         JPanel panel = new JPanel(new GridBagLayout());
-        JMenuBar menuBar = new JMenuBar();
-        JMenu firstMenu = new JMenu("firstMenu");
-        JMenuItem menuItem = new JMenuItem("testing here",KeyEvent.VK_R);
-        JMenuItem autor = new JMenuItem("o autorze",KeyEvent.VK_Q);
-        JToolBar toolBar = new JToolBar("takie se");
-        JButton left = new JButton("<--");
-        JButton right = new JButton("-->");
-        String [] columny = {"jeden","dwa","trzy","cztery","piec"};
-        Object[][] dane = {{"1","2","5","1","1"},{"3","4","6","1","1"},{"7","8","9","1","1"},{"7","8","9","1","1"},{"7","8","9","1","1"}};
-        kek = new JTable(dane,columny);
-        kek.setRowHeight(40);
+        String[] columny = {"jeden", "dwa", "trzy", "cztery", "piec"};
+        Object[][] dane = {{"3", "2", "5", "5", "5"}, {"3", "4", "6", "5", "5"}, {"7", "8", "9", "5", "5"}, {"7", "8", "9", "5", "5"}, {"7", "8", "9", "5", "10"}};
+        table = new JTable(dane, columny);
+        table.setRowHeight(40);
 
         c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(100,0,0,0,0,0,0,0);
-        panel.add(kek.getTableHeader(),c);
+        setConstraints(100, 0, 1, 0, 0, 0, 0, 0);
+        panel.add(table.getTableHeader(), c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(0,0,0,1,0,0,0,0);
-        panel.add(kek,c);
+        setConstraints(0, 0, 1, 1, 0, 0, 0, 0);
+        panel.add(table, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(0,0,1,0,0,0,0,0);
-        panel.add(textFieldLabel,c);
+        setConstraints(0, 0, 2, 0, 0, 0, 0, 0);
+        panel.add(textFieldLabel, c);
 
         c.fill = GridBagConstraints.BOTH;
-        setConstraints(0,0,1,1,0,0,0,0);
+        setConstraints(0, 0, 2, 1, 0, 0, 0, 0);
         c.gridheight = 2;
-        panel.add(textField,c);
+        panel.add(textField, c);
 
         c.gridheight = 1;
         c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(0,0,2,0,0,0,0,0);
-        panel.add(textAreaLabel,c);
+        setConstraints(0, 0, 3, 0, 0, 0, 0, 0);
+        panel.add(textAreaLabel, c);
 
         c.gridheight = 2;
         c.fill = GridBagConstraints.BOTH;
-        setConstraints(0,0,2,1,0,0,0,0);
+        setConstraints(0, 0, 3, 1, 0, 0, 0, 0);
         textArea.setEditable(false);
-        Font font = new Font("Times New Roma",Font.PLAIN,16);
+        Font font = new Font("Times New Roma", Font.PLAIN, 16);
         textArea.setForeground(Color.black);
         textArea.setFont(font);
-        panel.add(textArea,c);
+        panel.add(textArea, c);
 
-        clear.addActionListener(this);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(100,40,0,3,0,0,0,0);
-        panel.add(clear,c);
-
-        insert.addActionListener(this);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(100,40,2,3,0,0,0,0);
-        panel.add(insert,c);
-
-        save.addActionListener(this);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        setConstraints(100,40,1,3,0,0,0,0);
-        panel.add(save,c);
-
-        firstMenu.setMnemonic(KeyEvent.VK_T);
-        firstMenu.getAccessibleContext().setAccessibleDescription("to jest test");
-        menuBar.add(firstMenu);
-
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription("tutaj nic");
-        menuItem.addActionListener(this);
-        firstMenu.add(menuItem);
-        menuItem.getAccessibleContext().setAccessibleDescription("tutaj tez nic");
-        autor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,InputEvent.CTRL_MASK));
-        autor.addActionListener(this);
-        firstMenu.add(autor);
-        left.setActionCommand("<--");
-        left.setToolTipText("nie dziala");
-        left.addActionListener(this);
-        right.setActionCommand("-->");
-        right.setToolTipText("ten tez");
-        right.addActionListener(this);
         sliderHorizontal.setName("hor");
         sliderVertical.setName("ver");
         sliderHorizontal.setMinorTickSpacing(1);
@@ -112,21 +79,79 @@ public class Window implements ActionListener, ChangeListener {
         sliderVertical.addChangeListener(this);
         sliderVertical.setMinorTickSpacing(1);
         sliderVertical.setMajorTickSpacing(5);
-        toolBar.add(sliderVertical);
-        toolBar.add(sliderHorizontal);
-        toolBar.add(left);
+        sliderVertical.setPaintTicks(true);
+        sliderHorizontal.setPaintTicks(true);
+
+        c.gridwidth = 1;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        setConstraints(0, 0, 1, 2, 0, 0, 0, 0);
+        panel.add(sliderHorizontal, c);
+
+        c.gridheight = 2;
+        setConstraints(0, 0, 0, 0, 0, 0, 0, 0);
+        panel.add(sliderVertical, c);
+
+        //Menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu tools = new JMenu("Narzędzia");
+        JMenu about = new JMenu("About");
+        JMenuItem autor = new JMenuItem("o autorze", KeyEvent.VK_Q);
+        JMenuItem minItem = new JMenuItem("Minimum");
+        JMenuItem maxItem = new JMenuItem("Maximum");
+        JMenuItem averageItem = new JMenuItem("Average");
+
+        menuBar.add(tools);
+        menuBar.add(about);
+
+        autor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_MASK));
+        autor.addActionListener(this);
+        about.add(autor);
+
+        minItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_MASK));
+        minItem.addActionListener(this);
+        tools.add(minItem);
+
+        maxItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK));
+        maxItem.addActionListener(this);
+        tools.add(maxItem);
+
+        averageItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_MASK));
+        averageItem.addActionListener(this);
+        tools.add(averageItem);
+
+
+        //ToolBar
+        JToolBar toolBar = new JToolBar("takie se");
+        toolBar.setLayout(new GridBagLayout());
+        result = new JTextArea();
+        result.setPreferredSize(new Dimension(100,50));
+        result.setEditable(false);
+
         average.addActionListener(this);
         min.addActionListener(this);
         max.addActionListener(this);
+        clear.addActionListener(this);
+        save.addActionListener(this);
+        insert.addActionListener(this);
+        toolBar.add(clear);
+        toolBar.addSeparator(new Dimension(10, 10));
+        toolBar.add(save);
+        toolBar.addSeparator(new Dimension(10, 10));
+        toolBar.add(insert);
+        toolBar.addSeparator(new Dimension(10, 10));
         toolBar.add(min);
+        toolBar.addSeparator(new Dimension(10, 10));
         toolBar.add(max);
+        toolBar.addSeparator(new Dimension(10, 10));
         toolBar.add(average);
-        toolBar.add(right);
-        window.setSize(1024,768);
-        if(dimension.getWidth() <= window.getWidth() | dimension.getHeight() <= window.getHeight()){
-            window.setSize((int)dimension.getWidth()/2,(int)dimension.getHeight()/2);
+        toolBar.addSeparator(new Dimension(10, 10));
+        toolBar.add(result);
+
+        window.setSize(768, 480);
+        if (dimension.getWidth() <= window.getWidth() | dimension.getHeight() <= window.getHeight()) {
+            window.setSize((int) dimension.getWidth() / 2, (int) dimension.getHeight() / 2);
         }
-        window.setLocation(((int)dimension.getWidth()/2)-(window.getWidth()/2),((int)dimension.getHeight()/2)-((window.getWidth()))/2);
+        window.setLocation(((int) dimension.getWidth() / 2) - (window.getWidth() / 2), ((int) dimension.getHeight() / 2) - ((window.getWidth())) / 2);
         window.setLayout(new FlowLayout());
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setJMenuBar(menuBar);
@@ -138,101 +163,108 @@ public class Window implements ActionListener, ChangeListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         String name = e.getActionCommand();
-        switch (name){
-            case "<--":
-                System.out.println("lewoooo");
-
-                break;
-            case "-->":
-                System.out.println("prawoooo");
-                break;
+        switch (name) {
             case "o autorze":
-                System.out.println("autor");
-                JOptionPane.showMessageDialog(window,"aplikacje wykonal Daniel Kuć");
-                break;
-            case "testing here":
-                System.out.println("testingggg");
+                JOptionPane.showMessageDialog(window, "aplikacje wykonal Daniel Kuć");
                 break;
             case "clear":
-                for(int i = 0; i<kek.getRowCount();i++){
-                    for(int j = 0; j<kek.getColumnCount();j++){
-                        kek.setValueAt(null,i,j);
+                for (int i = 0; i < table.getRowCount(); i++) {
+                    for (int j = 0; j < table.getColumnCount(); j++) {
+                        table.setValueAt(null, i, j);
                     }
                 }
                 break;
             case "insert":
                 int ver = sliderVertical.getValue();
                 int hor = sliderHorizontal.getValue();
-                textArea.setText((String)kek.getValueAt(hor,ver));
-                kek.setValueAt(textField.getText(),hor,ver);
+                result.setText((String)table.getValueAt(ver, hor));
+                table.setValueAt(textField.getText(), ver, hor);
                 break;
             case "save":
-                break;
-            case "min":
-                int temp=0;
-                int tempk=0;
-                int min = 0;
-                for(int i = 0; i<kek.getRowCount();i++){
-                    for(int j = 0; j<kek.getColumnCount();j++){
-                        temp = Integer.parseInt((String)kek.getValueAt(i,j));
-                        if(tempk<temp){
-                            min = tempk;
-                        }else {
-                            min = temp;
+                try {
+                    FileWriter fileWriter = new FileWriter("./test.txt",true);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                    for (int i = 0; i < table.getRowCount(); i++) {
+                        for (int j = 0; j < table.getColumnCount(); j++) {
+                            bufferedWriter.write((String)table.getValueAt(i,j));
+                            bufferedWriter.write(" ");
                         }
-                        tempk = Integer.parseInt((String)kek.getValueAt(i,j));
-                        textArea.setText(Integer.toString(min));
+                        bufferedWriter.newLine();
+                    }
+                    bufferedWriter.close();
+                } catch (IOException ee) {
+                    ee.printStackTrace();
+                }
+                break;
+            case "Minimum":
+                int temp = 0;
+                int tempk = 0;
+                int min = 99999999;
+                for (int i = 0; i < table.getRowCount(); i++) {
+                    for (int j = 0; j < table.getColumnCount(); j++) {
+                        temp = Integer.parseInt((String)table.getValueAt(i, j));
+                        if ((j + 1) < table.getColumnCount()) {
+                            tempk = Integer.parseInt((String)table.getValueAt(i, j + 1));
+                        }
+                        if (temp < min) {
+                            min = temp;
+                        } else if (tempk < min) {
+                            min = tempk;
+                        }
+                        result.setText(Integer.toString(min));
                     }
                 }
                 break;
-            case "max":
-                temp=0;
-                tempk=0;
+            case "Maximum":
+                temp = 0;
+                tempk = 0;
                 int max = 0;
-                for(int i = 0; i<kek.getRowCount();i++){
-                    for(int j = 0; j<kek.getColumnCount();j++){
-                        temp = Integer.parseInt((String)kek.getValueAt(i,j));
-                        if(tempk>temp){
+                for (int i = 0; i < table.getRowCount(); i++) {
+                    for (int j = 0; j < table.getColumnCount(); j++) {
+                        temp = Integer.parseInt((String) table.getValueAt(i, j));
+                        if ((j + 1) < table.getColumnCount()) {
+                            tempk = Integer.parseInt((String) table.getValueAt(i, j + 1));
+                        }
+                        if (temp > max) {
+                            max = temp;
+                        } else if (tempk > max) {
                             max = tempk;
                         }
-                        tempk = Integer.parseInt((String)kek.getValueAt(i,j));
-                        textArea.setText(Integer.toString(max));
+                        result.setText(Integer.toString(max));
                     }
                 }
                 break;
             case "average":
                 int sum = 0;
                 int dzielnik = 0;
-                for(int i = 0; i<kek.getRowCount();i++){
-                    for(int j = 0; j<kek.getColumnCount();j++){
-                        sum+=Integer.parseInt((String)kek.getValueAt(i,j));
+                for (int i = 0; i < table.getRowCount(); i++) {
+                    for (int j = 0; j < table.getColumnCount(); j++) {
+                        sum += Integer.parseInt((String) table.getValueAt(i, j));
                         dzielnik++;
                     }
-                    textArea.setText(Integer.toString(sum/dzielnik));
+                    result.setText(Integer.toString(sum / dzielnik));
                 }
                 break;
         }
 
     }
-    public void setConstraints(int a, int b, int d, int e,int f, int g,int h, int o){
+
+    public void setConstraints(int a, int b, int d, int e, int f, int g, int h, int o) {
         c.ipadx = a; //ustawia szerokosc
         c.ipady = b; //ustawia wysokosc
         c.gridx = d; //ustawia pozycje x,y komorki w siatce
         c.gridy = e;
-        c.insets = new Insets(f,g,h,o); //ustawia paddingi miedzy komorkami (gora,lewo,dol,prawo)
+        c.insets = new Insets(f, g, h, o); //ustawia paddingi miedzy komorkami (gora,lewo,dol,prawo)
     }
 
     @Override
     public void stateChanged(ChangeEvent e) {
-        source = (JSlider)e.getSource();
-        System.out.println(source.getName());
-        int ver = 0;
-        int hor = 0;
-        if(source.getName().equals("ver")){
-            ver = source.getValue();
-        }else{
+        source = (JSlider) e.getSource();
+        if (source.getName().equals("ver")) {
+            ver = 4 - source.getValue();
+        } else {
             hor = source.getValue();
         }
-        textArea.setText((String)kek.getValueAt(hor,ver));
+        result.setText((String)table.getValueAt(ver, hor));
     }
 }
