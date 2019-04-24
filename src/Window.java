077bf52e129c3d383d5d665f7adca30397e07d6a
Ithilgore <daniel200825@wp.pl>
2019-04-24 03:04:39 +0200

    Merge branch 'test' of https://github.com/KDanielRepo/projektowanie
    # Conflicts:
    #	src/Window.java

--- src/Window.java
+++ src/Window.java
@@ -70,7 +70,7 @@ public class Window implements ActionListener, ChangeListener {
     Integer[][] dane = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
     TableModel tableModel = new TableModel(dane, columny);
     JFileChooser fileChooser = new JFileChooser();
-
+    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
     public Window() {
         createWindow();
         getValueList();
@@ -79,7 +79,6 @@ public class Window implements ActionListener, ChangeListener {
     }
 
     public void createWindow() {
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         panel = new JPanel(new GridBagLayout());
         table = new JTable(tableModel);
         table.setRowHeight(40);
@@ -152,7 +151,9 @@ public class Window implements ActionListener, ChangeListener {
 
         //Menu
         JMenuBar menuBar = new JMenuBar();
-        JMenu tools = new JMenu("Narzędzia");
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+        JMenu tools = new JMenu("Tools");
         JMenu about = new JMenu("About");
         JMenuItem autor = new JMenuItem("o autorze", KeyEvent.VK_Q);
         JMenuItem minItem = new JMenuItem("Minimum");
@@ -162,13 +163,20 @@ public class Window implements ActionListener, ChangeListener {
         JMenuItem clearItem = new JMenuItem("clear");
         JMenuItem insertItem = new JMenuItem("insert");
         JMenuItem saveItem = new JMenuItem("save");
+        JMenuItem help = new JMenuItem("help");
 
+        menuBar.add(file);
+        menuBar.add(edit);
         menuBar.add(tools);
         menuBar.add(about);
 
         autor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_MASK));
         autor.addActionListener(this);
         about.add(autor);
+
+        help.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_MASK));
+        help.addActionListener(this);
+        about.add(help);
 
         minItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_MASK));
         minItem.addActionListener(this);
@@ -188,15 +196,15 @@ public class Window implements ActionListener, ChangeListener {
 
         clearItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.SHIFT_MASK));
         clearItem.addActionListener(this);
-        tools.add(clearItem);
+        edit.add(clearItem);
 
         insertItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK));
         insertItem.addActionListener(this);
-        tools.add(insertItem);
+        edit.add(insertItem);
 
         saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.SHIFT_MASK));
         saveItem.addActionListener(this);
-        tools.add(saveItem);
+        file.add(saveItem);
 
 
         //ToolBar
@@ -224,7 +232,7 @@ public class Window implements ActionListener, ChangeListener {
         toolBar.setPreferredSize(new Dimension(window.getWidth(), 30));
         toolBar.setBackground(Color.lightGray);
 
-        window.setSize(768, 600);
+        window.setSize(800, 600);
         window.setLocation(((int) dimension.getWidth() / 2) - (window.getWidth() / 2), ((int) dimension.getHeight() / 2) - ((window.getWidth())) / 2);
         window.setLayout(new BorderLayout());
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -253,9 +261,9 @@ public class Window implements ActionListener, ChangeListener {
         mainPanel.add(tabbedPane);
 
         //outlookbar
-        JPanel testttt = new JPanel();
-        testttt.setPreferredSize(new Dimension(50, 50));
-        outlookBar.addTab("one", null, testttt);
+        JPanel outlook = new JPanel();
+        outlook.setPreferredSize(new Dimension(50, 50));
+        outlookBar.addTab("one", null, outlook);
         outlookBar.setVisible(true);
 
         //Pasek Statusu
@@ -267,7 +275,7 @@ public class Window implements ActionListener, ChangeListener {
         //Tips
         tipModel.add(new DefaultTip("1", "Kapibary są fajne"));
         tipModel.add(new DefaultTip("2", "I lubią pływać"));
-        tipModel.add(new DefaultTip("3", "Są bardzo faj"));
+        tipModel.add(new DefaultTip("3", "Są bardzo fajne"));
         tipModel.add(new DefaultTip("4", "kapibara"));
 
         //Dodawanie do ramki
@@ -278,6 +286,7 @@ public class Window implements ActionListener, ChangeListener {
         window.add(status, BorderLayout.PAGE_END);
         window.setVisible(true);
 
+        //logger
         BasicConfigurator.configure();
         try {
             FileAppender fileAppender = new FileAppender(patternLayout, "filelogs.log");
@@ -290,7 +299,8 @@ public class Window implements ActionListener, ChangeListener {
         logger.info("start");
 
     }
-
+        //Informacje o autorze w nowym oknie nr. versji, nazwa aplikacji, autor, licencja
+    //dodac help
     public void chart() {
         Platform.runLater(new Runnable() {
             @Override
@@ -347,6 +357,60 @@ public class Window implements ActionListener, ChangeListener {
         }
     }
 
+    public void showAuthor(){
+        JFrame jFrame = new JFrame("About Me");
+        JPanel jPanel = new JPanel(new GridLayout(5,1));
+        JLabel apk = new JLabel("Aplikacja testowa");
+        JLabel ver = new JLabel("Nr. wersji: 1.0.0");
+        JLabel lic = new JLabel("Licencja Freeware");
+        JLabel me = new JLabel("Twórca: Daniel Kuć");
+        JLabel con = new JLabel("Kontakt: Daniel200825@wp.pl");
+        jFrame.add(jPanel);
+        jPanel.add(apk);
+        jPanel.add(ver);
+        jPanel.add(lic);
+        jPanel.add(me);
+        jPanel.add(con);
+        jFrame.setSize(300,300);
+        jFrame.setLocation(((int) dimension.getWidth() / 2) - (window.getWidth() / 2), ((int) dimension.getHeight() / 2) - ((window.getWidth())) / 2);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+    }
+    public void showHelp(){
+        JFrame jFrame = new JFrame();
+        JPanel jPanel = new JPanel(new FlowLayout());
+        JTabbedPane jTabbedPane = new JTabbedPane();
+        jPanel.setSize(200,200);
+        JLabel apk = new JLabel("Aplikacja testowa");
+        JLabel ver = new JLabel("Nr. wersji: 1.0.0");
+        JLabel lic = new JLabel("Licencja Freeware");
+        JLabel me = new JLabel("Twórca: Daniel Kuć");
+        JPanel fileHelp = new JPanel();
+        jPanel.add(apk);
+        fileHelp.add(apk);
+        JPanel editHelp = new JPanel();
+        editHelp.add(ver);
+        JPanel toolsHelp = new JPanel();
+        toolsHelp.add(lic);
+        JPanel aboutHelp = new JPanel();
+        aboutHelp.add(me);
+        tabbedPane.addTab("file", null,fileHelp);
+        tabbedPane.addTab("edit", null,editHelp);
+        tabbedPane.addTab("tools", null,toolsHelp);
+        tabbedPane.addTab("about", null,aboutHelp);
+        tabbedPane.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+            }
+        });
+        jFrame.add(jPanel);
+        jPanel.add(jTabbedPane);
+        jFrame.setSize(300,300);
+        jFrame.setLocation(((int) dimension.getWidth() / 2) - (window.getWidth() / 2), ((int) dimension.getHeight() / 2) - ((window.getWidth())) / 2);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+    }
+
     public void setConstraints(int a, int b, int d, int e, int f, int g, int h, int o) {
         c.ipadx = a; //ustawia szerokosc
         c.ipady = b; //ustawia wysokosc
@@ -361,19 +425,27 @@ public class Window implements ActionListener, ChangeListener {
         System.out.println(name);
         switch (name) {
             case "o autorze":
-                JOptionPane.showMessageDialog(window, "aplikację wykonał Daniel Kuć");
+                //JOptionPane.showMessageDialog(window, "aplikację wykonał Daniel Kuć");
+                showAuthor();
+                logger.info("o autorze");
+                break;
+            case "help":
+                showHelp();
                 break;
             case "clear":
                 Object[] wybor = {"Tak", "Nie"};
                 int choise = JOptionPane.showOptionDialog(window, "Czy na pewno chcesz wyczyścić całą tabelę?", "Pytanie", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, wybor, wybor[1]);
                 if (choise == 0) {
                     tableModel.clearTableValues();
+                    logger.info("table cleared");
                 } else if (choise == 1) {
+                    logger.info("clear aborted");
                     return;
                 }
                 break;
             case "insert":
                 tableModel.insertIntoTable(ver, hor, Integer.parseInt(textField.getText()));
+                logger.info(Integer.parseInt(textField.getText())+" value inserted");
                 break;
             case "save":
                 File file = new File("./test.txt");
@@ -382,34 +454,64 @@ public class Window implements ActionListener, ChangeListener {
                 int fileChoise = fileChooser.showSaveDialog(window);
                 if (fileChoise == JFileChooser.APPROVE_OPTION) {
                     try {
-                        FileWriter fileWriter = new FileWriter(fileChooser.getSelectedFile(), true);
-                        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                        for (int i = 0; i < tableModel.getRowCount(); i++) {
-                            for (int j = 0; j < tableModel.getColumnCount(); j++) {
-                                bufferedWriter.write((Integer.toString((Integer) tableModel.getValueAt(i, j))));
-                                bufferedWriter.write(" ");
+                        if(fileChooser.getSelectedFile().exists()){
+                            Object[] yesNo = {"Tak", "Nie"};
+                            int choisee = JOptionPane.showOptionDialog(window, "Plik o takiej nazwie już istnieje, czy nadpisać go?", "Pytanie", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, yesNo, yesNo[1]);
+                            if (choisee == 0) {
+                                fileChooser.getSelectedFile().createNewFile();
+                                FileWriter fileWriter = new FileWriter(fileChooser.getSelectedFile());
+                                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                                for (int i = 0; i < tableModel.getRowCount(); i++) {
+                                    for (int j = 0; j < tableModel.getColumnCount(); j++) {
+                                        bufferedWriter.write((Integer.toString((Integer) tableModel.getValueAt(i, j))));
+                                        bufferedWriter.write(" ");
+                                    }
+                                    bufferedWriter.newLine();
+                                }
+                                logger.info("File overriten and saved!");
+                                textArea.setText("File Saved!");
+                                bufferedWriter.close();
+                            } else if (choisee == 1) {
+                                return;
                             }
-                            bufferedWriter.newLine();
+                        }else{
+                            fileChooser.getSelectedFile().createNewFile();
+                            FileWriter fileWriter = new FileWriter(fileChooser.getSelectedFile());
+                            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                            for (int i = 0; i < tableModel.getRowCount(); i++) {
+                                for (int j = 0; j < tableModel.getColumnCount(); j++) {
+                                    bufferedWriter.write((Integer.toString((Integer) tableModel.getValueAt(i, j))));
+                                    bufferedWriter.write(" ");
+                                }
+                                bufferedWriter.newLine();
+                            }
+                            logger.info("File saved!");
+                            textArea.setText("File Saved!");
+                            bufferedWriter.close();
                         }
-                        bufferedWriter.close();
-                    } catch (IOException | NumberFormatException ee) {
+                    } catch (IOException ee) {
                         ee.printStackTrace();
-                        textArea.setText("W tabeli nie znajdują się liczby lub są one niepoprawnie zapisane");
+                        textArea.setText("File save failed due to an unexpected Error");
                     }
                 }else if(fileChoise == JFileChooser.CANCEL_OPTION){
+                    logger.info("save aborted");
                     textArea.setText("Save Aborted");
                 }
                 break;
             case "Minimum":
                 textArea.setText(Integer.toString((Integer) tableModel.showMinimum()));
+                logger.info("Showed Minimum");
                 break;
             case "Maximum":
                 textArea.setText(Integer.toString((Integer) tableModel.showMaximum()));
+                logger.info("Showed Maximum");
                 break;
             case "average":
+                logger.info("Showed Average");
                 textArea.setText(Integer.toString((Integer) tableModel.showAverage()));
                 break;
             case "Sum":
+                logger.info("Showed Sum");
                 textArea.setText(Integer.toString((Integer) tableModel.showSum()));
                 break;
             case "comboBoxChanged":
@@ -418,15 +520,19 @@ public class Window implements ActionListener, ChangeListener {
                 switch (cb) {
                     case "Min":
                         textArea.setText(Integer.toString((Integer) tableModel.showMinimum()));
+                        logger.info("Showed minimum");
                         break;
                     case "Max":
                         textArea.setText(Integer.toString((Integer) tableModel.showMaximum()));
+                        logger.info("Showed Maximum");
                         break;
                     case "Average":
                         textArea.setText(Integer.toString((Integer) tableModel.showAverage()));
+                        logger.info("Showed Average");
                         break;
                     case "Sum":
                         textArea.setText(Integer.toString((Integer) tableModel.showSum()));
+                        logger.info("Showed Sum");
                         break;
                 }
                 break;
